19ffcc9a9c56f4880641bc688b237d8e93289f42
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-10-12 12:58:22 +0530

    initial commit

---
+++ bin/oops/encapsulation/mydata_use.dart
@@ -0,0 +1,11 @@
+import 'mydata.dart';
+
+void main(){
+  MyData obj = MyData();
+  obj.value_of_mark = 8;
+  obj.value_of_name = "Bibin";
+  print(obj.value_of_name);
+  print(obj.value_of_age);
+  print(obj.value_of_mark);
+
+}
